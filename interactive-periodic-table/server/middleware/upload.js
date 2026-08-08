1726edc41c9b7daba85204576383ba6be8b371f7
AryanDevHub <dandotiyaaryan2010@gmail.com>
2026-08-08 11:18:22 +0530

    Remove stale duplicate nested folder from old repo merge

--- interactive-periodic-table/server/middleware/upload.js
+++
@@ -1,8 +0,0 @@
-// FILE: server/middleware/upload.js
-const multer = require('multer');
-
-// Configure multer to store files in memory as buffers
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-module.exports = upload;
